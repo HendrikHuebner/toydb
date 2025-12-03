f06c901d5f6c882940da8fda4dd27d5b72fc03fc
hhuebner <hendrik.huebner18@gmail.com>
2025-12-03 14:13:25 +0100

    add includes for debugging

--- src/toydb.cpp
+++ src/toydb.cpp
@@ -1,12 +1,11 @@
 #include "toydb.hpp"
 #include "parser/query_ast.hpp"
-#include "engine/table.hpp"
+#include "engine/relation.hpp"
 #include "planner/query_plan.hpp"
 #include "engine/memory.hpp"
 #include "common/logging.hpp"
 #include "common/types.hpp"
-#include "engine/database.hpp"
-
+#include "engine/join.hpp"
 
 void foo() {
     toydb::ast::Literal l("foo");
